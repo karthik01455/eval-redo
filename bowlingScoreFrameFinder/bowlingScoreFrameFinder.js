2d0c31c131599bc0830fa3ab5a1eb1aa922639b0
karthik0145 <karthikshriram5111@gmail.com>
2023-01-18 18:27:48 +0530

    fix: fixed for edge cases

--- bowlingScoreFrameFinder/bowlingScoreFrameFinder.js
+++ bowlingScoreFrameFinder/bowlingScoreFrameFinder.js
@@ -1,109 +1,106 @@
+
 function findScore(rolls)
 {
     function calculateFrame(element,index,array)
-    { if(index<=20)
-    {
-        if(count == 0 && element!= 10)
-        {
-            count=1;
-            sum=sum+element;
-            result=result+element;
-            temporaryFrame.push(element);
-        }else if(count == 1)
-        {
-            sum=sum+element;
-            if(sum == 10)
-            {
-                result= result+ element;
-                result= result+ array[index+1];
+    {   
+        if (index <= 20) {
+            if (count == 0 && element != 10) {
+                count = 1;
+                sum = sum + element;
+                result = result + element;
                 temporaryFrame.push(element);
-                temporaryFrame.push(array[index+1]);
+            } else if (count == 1 && element != 10) {
+                sum = sum + element;
+                if (sum == 10) {
+                    result = result + element;
+                    result = result + array[index + 1];
+                    temporaryFrame.push(element);
+                    temporaryFrame.push(array[index + 1]);
+                }
+                else {
+                    result = result + element;
+                    temporaryFrame.push(element);
+                }
+                frame.push(temporaryFrame);
+                temporaryFrame = [];
+                count = 0;
+                sum = 0;
+                frameLength =frameLength +1;
             }
-            else
-            {
-                result=result+element;
+            else if (count == 0 && element == 10 && index < array.length - 3) {
+                sum = 0;
+                result = result + 10;
+                result = result + array[index + 1];
+                result = result + array[index + 2];
+                count = 0;
                 temporaryFrame.push(element);
+                temporaryFrame.push(array[index + 1]);
+                temporaryFrame.push(array[index + 2]);
+                frame.push(temporaryFrame);
+                temporaryFrame = [];
+                frameLength =frameLength +1;
             }
-            frame.push(temporaryFrame);
-            temporaryFrame=[];
-            count=0;
-            sum=0;
-        }
-        else if(count==0 && element == 10 && index<array.length-2)
-        {
-            sum=0;
-            result = result + 10 ;
-            result= result+array[index+1];
-            result=result+ array[index+2];
-            count=0;
-            temporaryFrame.push(element);
-            temporaryFrame.push(array[index+1]);
-            temporaryFrame.push(array[index+2]);
-            frame.push(temporaryFrame);
-            temporaryFrame=[];
+
         }
 
     }
-
-    }
-    let frame=[];
-    let result=0;
-    let count=0;
-    let sum=0;
-    let temporaryFrame=[];
+    let frame = [];
+    let result = 0;
+    let count = 0;
+    let sum = 0;
+    let frameLength = 0;
+    let temporaryFrame = [];
     rolls.forEach(calculateFrame);
+    console.log('Frame length '+frameLength);
+    if(frameLength<9)
+    {
+        throw new Error('Invalid Input');
+    }
     return result;
 }
-function findFrame(rolls)
-{
-    function calculateFrame(element,index,array)
-    { if(index<=10)
-    {
-        if(count==0 && element!=10)
-        {
-            count=1;
-            sum=sum+element;
-            result=result+element;
-            temporaryFrame.push(element);
-        }else if(count == 1)
-        {
-            sum=sum+element;
-            if(sum == 10)
-            {
-                result= result+ element;
-                result= result+ array[index+1];
+function findFrame(rolls) {
+    function calculateFrame(element, index, array) {
+        if (index <= 20) {
+            if (count == 0 && element != 10) {
+                count = 1;
+                sum = sum + element;
+                result = result + element;
                 temporaryFrame.push(element);
-                temporaryFrame.push(array[index+1]);
+            } else if (count == 1) {
+                sum = sum + element;
+                if (sum == 10) {
+                    result = result + element;
+                    result = result + array[index + 1];
+                    temporaryFrame.push(element);
+                    temporaryFrame.push(array[index + 1]);
+                }
+                else {
+                    result = result + element;
+                    temporaryFrame.push(element);
+                }
+                frame.push(temporaryFrame);
+                temporaryFrame = [];
+                count = 0;
+                sum = 0;
             }
-            else
-            {
-                result=result+element;
+            else if (count == 0 && element == 10 && index < array.length-4) {
+                sum = 0;
+                result = result + 10;
+                result = result + array[index + 1];
+                result = result + array[index + 2];
+                count = 0;
                 temporaryFrame.push(element);
+                temporaryFrame.push(array[index + 1]);
+                temporaryFrame.push(array[index + 2]);
+                frame.push(temporaryFrame);
+                temporaryFrame = [];
             }
-            frame.push(temporaryFrame);
-            temporaryFrame=[];
-            count=0;
-            sum=0;
-        }
-        else if(count==0 && element == 10 )
-        {
-            sum=0;
-            result = result + 10 ;
-            result= result+array[index+1];
-            result=result+ array[index+2];
-            count=0;
-            temporaryFrame.push(element);
-            temporaryFrame.push(array[index+1]);
-            temporaryFrame.push(array[index+2]);
-            frame.push(temporaryFrame);
-            temporaryFrame=[];
+
         }
 
     }
-
-    }
-    let frame=[];
-    let result=0;
+    let frame = [];
+    let result = 0;
     let count=0;
     let sum=0;
     let temporaryFrame=[];
@@ -121,16 +118,12 @@ function getMaxScore(rollsArray)
     rollsArray.forEach(findMaxScore);
     return maxScore;
 }
-console.log(findScore([10,10,10,10,10,10,10,10,10,10,10,10]));
+console.log(findScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]));
 console.log(findScore([3,7,3,7,1,1]));
-console.log(findFrame([10,10,10,10,10,10,10,10,10,10,5,5]));
-console.log(findFrame([3,7,3,7,1,1]));
-console.log(findScore([10,10,10,10,10,10,10,10,10,10,5,5]));
-console.log(findScore([3,7,3,7,1,1]));
-
-console.log(findFrame([10,10,10,10,10,10,10,10,10,10,5,5]));
+console.log(findScore([10,10,10,10,10,10,10,10,10,10,10,7]));
+//console.log(findScore([3,7,3,7,1,1]));
 console.log('.');
 console.log(getMaxScore([[10,10,10,10,10,10,10,10,10,10,10,10],[10,10,10,10,10,10,10,10,10,10,1,1]]));
 console.log('.');
 console.log(findFrame([3,7,3,7,1,1]));
-module.exports ={ findScore, findFrame };
+module.exports ={ findScore, findFrame,getMaxScore };
